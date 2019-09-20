5edce83eee5d79602ed31a6faf23485740191ea8
Nick Hartford <skyler0121@gmail.com>
2019-09-19 22:37:20 -0400

    q

--- app.py
+++ app.py
@@ -1,1 +1,27 @@
-print("hello world")
+# message = "Hello!"
+
+
+# secMessage = "Nick"
+
+# print(f"{message}{secMessage}")
+
+# message = "Hello nick, would you like to learn some python today?"
+
+# print(message)
+
+# pName = "nick"
+
+# print(pName.title())
+# print(pName.upper())
+# print(pName.lower())
+
+# famous_person = " henry ford "
+
+# quote = f'{famous_person.title().strip()} once said, "Whether you think you\n\t can or you think you cant, your right"'
+
+# print(quote)
+
+# print(5+3)
+# print(4*2)
+# print(16/2)
+# print(10-2)
