1ba3f83d8cfb179c4e361e317671c750a4b7c7f2
Nick Hartford <skyler0121@gmail.com>
2019-10-02 09:03:33 -0400

    lotto game

--- lotty.py
+++ lotty.py
@@ -1,5 +1,39 @@
 from random import choice
 
-numbers = [10, 12, 3, 45, 6, 73, 24, 'n', 'r', 't', 'w', 'k']
+
+
+numbers = [10, 12, 3, 45, 26, 65, 29]
+attepmts = 0
+guesses = 0
+winner = [12, 3, 26, 10]
+guess = []
+
+while True:
+
+    while attepmts < 4:
+
+        guess.append(choice(numbers))
+        attepmts += 1
+
+
+    print(guess)
+
+    # print(sorted(guess))
+
+    if sorted(winner) == sorted(guess):
+        print('Winner, Winner chicken dinner')
+        guesses += 1
+        break
+    else:
+        guesses += 1
+        guess = []
+        attepmts = 0
+        
+
+print(guesses)
+
+
+
+
 
 
