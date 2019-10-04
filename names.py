f608bdc962331fd17ec7fd2888bc57af4521d32f
Nick Hartford <skyler0121@gmail.com>
2019-10-04 09:11:18 -0400

    added text files

---
+++ names.py
@@ -0,0 +1,8 @@
+file_path = "text_files/names.txt"
+names = ['nick', 'juletta', 'skyler', 'Amelia']
+
+with open(file_path, 'a') as file_object:
+    for name in names:
+        file_object.write('\n' + name)
+
+print('all done')
