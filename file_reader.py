477ffcd6ed5980690778f30d83274e640106a360
Nick Hartford <skyler0121@gmail.com>
2019-10-04 21:00:34 -0400

    ch 10 complete

--- file_reader.py
+++ file_reader.py
@@ -41,5 +41,105 @@ file_path = 'text_files/digits.txt'   ############## path to txt file
 #     print(f'Welcome {users_name}')
 #     with open(file_path, 'a') as file_object:
 #         file_object.write('\n' + users_name)
+# try:
+#     print(50/0)
+# except ZeroDivisionError:
+#     print('You cant divide by zero silly')
 
+# def add_values(val1, val2):
+#     return val1 + val2
+
+# num_times = 0
+# value1 = 0
+# value2 = 0
+
+# while num_times < 2:
+#     prompt = input('please enter two values to add')
+#     num_times += 1
+#     if prompt == 'q':
+#         break
+#     elif num_times == 1:
+#         try:
+#             value1 = int(prompt)
+#         except ValueError:
+#             print('wrong type')
+#             num_times -= 1
+#     elif num_times == 2:
+#         try:
+#             value2 = int(prompt)
+#         except ValueError:
+#             print('wrong type')
+#             num_times -= 1
+#     elif value1 != 0 and value2 != 0:
+#         continue
     
+
+# try:
+#     print(add_values(int(value1), int(value2)))
+# except ValueError:
+#     print('please enter a valid value from 0 - 9')
+#     print('please try again')
+
+# filenames = ['cats.txt', 'bags.txt', 'dogs.txt']
+
+# def check_files(filename):
+#     try:
+#         with open(filename, 'r') as f:
+#             contents = f.read()
+#             print("\n" + contents)
+#     except FileNotFoundError:
+#         pass
+
+
+
+
+# for filename in filenames:
+#     check_files(filename)
+    
+# import json
+
+# filename = "numbers.json"
+
+# numbers = [2,4,6,3,12,45,76,23]
+
+# with open(filename) as f:
+#     json.load(f)
+
+# print(numbers)
+# filename = 'username.json'
+
+# try:
+#     with open(filename) as f:
+#         username = json.load(f)
+# except FileNotFoundError:
+#     username = input('what is your name')
+#     with open(filename, 'w') as f:
+#         json.dump(username, f)
+#         print(f'we will remember you next time {username}')
+# else:
+#     print(f'welcome back, {username}')        ###################### This block runs when "try" is successful
+
+# filename = "favorite.json"
+
+# # favorite_number = input('whats your favorite number')
+
+
+# # with open(filename, 'w') as f:
+# #     json.dump(favorite_number, f)
+
+# try:
+#     with open(filename) as f:
+#         number = json.load(f)
+# except FileNotFoundError:
+#     print('file not found')
+# else:
+#     print(f'I know your favorite number, its {number}')
+
+
+
+
+
+
+        
+
+
--- names.py
+++ names.py
@@ -1,8 +1,8 @@
-file_path = "text_files/names.txt"
-names = ['nick', 'juletta', 'skyler', 'Amelia']
+# file_path = "text_files/names.txt"
+# names = ['nick', 'juletta', 'skyler', 'Amelia']
 
-with open(file_path, 'a') as file_object:
-    for name in names:
-        file_object.write('\n' + name)
+# with open(file_path, 'a') as file_object:
+#     for name in names:
+#         file_object.write('\n' + name)
 
-print('all done')
+# print('all done')
