5fc7d8b862df95426ce3056c0fa9383e0a4271fa
Nick Hartford <skyler0121@gmail.com>
2019-10-07 18:08:07 -0400

    practice pygame

--- Part_2/practice_game/alien_invasion.py
+++ Part_2/practice_game/alien_invasion.py
@@ -9,18 +9,20 @@ class AlienInvasion:
        window_width = 1200
        window_height = 800
 
-       self.screen = pygame.display.set_mode(window_width, window_height)
+       self.screen = pygame.display.set_mode((window_width, window_height))
 
        pygame.display.set_caption("Alien Invasion") 
+
+       self.bg_color = (230, 230, 230)
 
     
     def run_game(self):
         while True:
             for event in pygame.event.get():
-                if event.type() == pygame.QUIT:
+                if event.type == pygame.QUIT:
                     sys.exit()
             
-            self.screen.fill(230,230,230)
+            self.screen.fill(self.bg_color)
 
             pygame.display.flip()
 
