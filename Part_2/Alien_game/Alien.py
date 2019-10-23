d4ccaed331f863acacdfafb917778d06c18a4928
Nick Hartford <skyler0121@gmail.com>
2019-10-23 19:39:02 -0400

    moving aliens

--- Part_2/Alien_game/Alien.py
+++ Part_2/Alien_game/Alien.py
@@ -6,6 +6,7 @@ class Alien(Sprite):
     def __init__(self, ai_game):
         super().__init__()
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
 
         #load the alien and set its rect attribute
         self.image = pg.image.load('D:\\programming\\Dev\\python\\practice\\Python_sandbox\\python_practice\\Part_2\\alien_invasion\\img\\alien.bmp')
@@ -17,3 +18,22 @@ class Alien(Sprite):
 
         #store the aliens exact horizontal positoion
         self.x = float(self.rect.x)
+
+
+
+    def check_edges(self):
+        #return true if alien is at edge of screen
+        screen_rect = self.screen.get_rect()
+
+        if self.rect.right >= screen_rect.right or self.rect.left <= 0:
+            return True
+
+
+    def update(self):
+
+        #Move alien right or left
+        self.x += (self.settings.alien_speed * self.settings.fleet_direction)
+
+        self.rect.x = self.x
+
+        
